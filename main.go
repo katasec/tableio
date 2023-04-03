cbe6ab66170158dad11108e1a6ff7b15ae56f2cc
Ameer Deen <ameer.deen@qigroup.com>
2023-04-03 22:16:31 +0400

    Removed need for sqlx. Using reflection for database/sql row scans

--- main.go
+++ main.go
@@ -13,10 +13,11 @@ func main() {
 	shapeTable, _ := NewTableIO[Shape]("sqlite3", "test.db")
 	shapeTable.CreateTableIfNotExists()
 
-	shapeTable.Insert(Shape{Width: 10, Height: 20, Color: "red"})
+	shapeTable.Insert(Shape{Width: 110, Height: 210, Color: "yellow"})
 
 	shapes := shapeTable.All()
 
+	//fmt.Print(shapes.Color)
 	for i, shape := range shapes {
 		fmt.Printf("%d. Color:%s, Height:%d, Width:%d \n", i, shape.Color, shape.Height, shape.Width)
 	}
